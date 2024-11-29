997c0bf5fdbfd48c64f0145b94ca287bd6a04a19
Art1488em <artemdavydov401@gmail.com>
2024-11-29 17:06:41 +0500

    Add files via upload

---
+++ src/components/Brands/Brands.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Brands.scss'
+
+const Brands = () => {
+  return (
+    <>
+    <div className="brands__imgs">
+        <img src="/brands-1.svg" alt="" />
+        <img src="/brands-2.svg" alt="" />
+        <img src="/brands-3.svg" alt="" />
+        <img src="/brands-4.svg" alt="" />
+        <img src="/brands-5.svg" alt="" />
+    </div>
+    </>
+  )
+}
+
+export default Brands
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Hero.scss'
+
+const Hero = () => {
+  return (
+    <>
+    <div className="section">
+      <div className="container">
+        <div className="gigawrapper">
+        <div className="wrapper">
+          <h1>FIND CLOTHES <br /> THAT MATCHES <br /> YOUR STYLE </h1>
+          <p>Browse through our diverse range of meticulously crafted garments, designed <br /> to bring out your individuality and cater to your sense of style.</p>
+          <button>Shop Now</button> <br />
+          <img src="./xuynya.png" alt="" />
+          
+        </div>
+
+        <img src="./hero-img.png" alt="" />
+        </div>
+        
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Hero
